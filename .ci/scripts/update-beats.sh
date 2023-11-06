b505af3cd2132cf380b2550d4707a7408b43bdc4
Orestis Floros <orestis.floros@elastic.co>
2023-11-06 20:03:15 +0100

    WIP: beats

--- .ci/scripts/update-beats.sh
+++ .ci/scripts/update-beats.sh
@@ -2,5 +2,13 @@
 set -euxo pipefail
 
 BEATS_VERSION=${1:?Missing version argument}
+
+go version
+go env
+env
+
 go get "github.com/elastic/beats/v7@$BEATS_VERSION"
 go mod tidy
+
+cat go.mod
+git diff
