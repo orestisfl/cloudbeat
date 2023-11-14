309d4331066cd606c53dc64539c3ec00a1ffd631
Orestis Floros <orestis.floros@elastic.co>
2023-11-14 09:59:37 +0100

    WIP

--- .ci/updatecli/scripts/update-hermit.sh
+++ .ci/updatecli/scripts/update-hermit.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 set -euxo pipefail
 
-bin/hermit upgrade \
-    aws-iam-authenticator \
-    awscli \
-    elastic-package \
-    gcloud \
-    gh \
-    golangci-lint \
-    jq \
-    just \
-    kind \
-    mage \
-    opa \
-    pre-commit \
-    rain \
-    regal \
-    shellcheck \
-    shfmt \
+packages=(
+    aws-iam-authenticator
+    awscli
+    elastic-package
+    gcloud
+    gh
+    golangci-lint
+    jq
+    just
+    kind
+    mage
+    opa
+    pre-commit
+    rain
+    regal
+    shellcheck
+    shfmt
     yq
+)
+
+# `hermit upgrade` will only upgrade minor versions for packages that use semantic versioning. Not all of our hermit
+# packages use semantic versioning and even for those that do, we want to be at least aware of new major versions and
+# update to those if they don't cause too much of a breakage.
+# Uninstalling and re-installing will always install to the latest major version.
+bin/hermit uninstall "${packages[@]}"
+bin/hermit install "${packages[@]}"
+bin/hermit list
 
 # Update pre-commit hooks
 pre-commit autoupdate
